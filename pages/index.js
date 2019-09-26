d24cfc07e683a275b5fe3a5afffb719b9660c0e6
Neville Dabre <ndabre@myeffect.ca>
2019-09-26 14:36:50 -0400

    Master Initialize

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Layout from '../components/Layout'
+import WithLayout from '../components/WithLayout'
+
+const Index = () => (
+        <p>Hello World</p>
+);
+
+export default WithLayout(Index)
